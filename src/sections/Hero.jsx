e0faca78a1510bdd5d7f1c8618b3db9e3b8805d3
AhmedZeer <ahmed.zeer@std.yildiz.edu.tr>
2024-02-10 19:20:30 +0300

    heroSecton

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,3 +1,4 @@
+import { bigShoe1 } from "../assets/images"
 import { statistics } from "../../constants"
 import {arrowRight} from "../assets/icons"
 import Button from "../components/Button"
@@ -17,11 +18,21 @@ const Hero = () => {
         <h1 className="mt-10 font-palanquin font-bold
                       text-8xl max-sm:text-[50px]
                       max-sm:leading-[1]">
-          <span>Best title </span>
+          <span
+          className="xl:bg-white
+          xl:whitespace-nowrap
+          relative z-5 pr-10"
+          >check out this shoe</span>
           <br/>
-          <span>EVER.</span>
+          <span 
+          className="text-coral-red
+          inline-block mt-3"
+          >it looks cool.</span><br/>I guess.
         </h1>
-        <p>
+        <p className="font-montserrat
+        text-slate-gray text-lg
+        leading-8 mt-6 mb-14
+        sm:max-w-sm">
           this is the best description
           ever to put on a hero for 
           your website xd.
@@ -32,14 +43,28 @@ const Hero = () => {
                         flex-wrap w-full mt-20 gap-16">
           {statistics.map((stat,index) => (
             <div>
-              <p>{stat.value}</p>
-              <p>{stat.label}</p>
+              <p className="text-4xl font-palanquin
+                  font-bold">{stat.value}</p>
+              <p className="leading-7 font-montserrat
+               text-slate-gray font-light">{stat.label}</p>
             </div>
           ))}
         </div>
         <h1>hello</h1>
         <h1>hello</h1>
         <h1>hello</h1>
+      </div>
+      <div className="flex relative
+      items-center flex-1 justify-center
+      xl:min-h-screen max-xl:py-40 bg-primary
+      bg-hero">
+        <img className="object-contain relative"
+          src = { bigShoe1 }
+          alt = "showimage"
+          width={610}
+          height={500}
+          z-10
+        />
       </div>
     </section>
   )
